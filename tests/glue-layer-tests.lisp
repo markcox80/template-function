e9a21781b542e6f5d944c1731dda543f7bf56cf5
Mark Cox <markcox80@gmail.com>
2017-06-04 16:00:27 +1000

    Added glue-layer-test basic.

--- tests/glue-layer-tests.lisp
+++ tests/glue-layer-tests.lisp
@@ -1,3 +1,26 @@
-(glue-layer-test example
+(glue-layer-test basic
+  (eval-when (:compile-toplevel :load-toplevel)
+    (defun example-function-type (x y alpha)
+      `(function (,x ,y ,alpha) ,y))
+
+    (defun example-lambda-form (<x> <y> <alpha>)
+      `(lambda (x y alpha)
+         (check-type x ,<x>)
+         (check-type y ,<y>)
+         (check-type alpha ,<alpha>)
+         (dotimes (i (min (length x) (length y)))
+           (setf (elt y i) (+ (* alpha (elt x i))
+                              (elt y i))))
+         y))
+
+    (template-function:ensure-template-function 'example '(x y alpha)
+                                                :lambda-form-function #'example-lambda-form
+                                                :function-type-function #'example-function-type)
+
+    (template-function:ensure-instantiation 'example '(array array number)))
+
   (test example
-    (is-true t)))
+    (let* ((x (make-array 5 :initial-element 1))
+           (y (make-array 5 :initial-element 2)))
+      (example (the array x) (the array y) 1)
+      (is (equalp #(3 3 3 3 3) y)))))
